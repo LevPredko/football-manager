4889ab96ab7e7411b0faed569729b46ed3f3c55b
LevPredko <levpredko@gmail.com>
2025-01-21 01:21:06 +0200

    created models

---
+++ src/main/java/ua/predko/footballManager/model/Team.java
@@ -0,0 +1,18 @@
+package ua.predko.footballManager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double balance;
+    private int commission;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Player> players;
+}
