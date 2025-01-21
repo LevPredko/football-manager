53dddcb8297004f4986efe337842f9be42d66178
LevPredko <levpredko@gmail.com>
2025-01-21 02:30:45 +0200

    added a data.sql file to initialize the data,

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO team (name, balance, commission) VALUES ('Team A', 1000000, 5);
+INSERT INTO team (name, balance, commission) VALUES ('Team B', 2000000, 3);
+
+INSERT INTO player (name, age, experience_months, team_id) VALUES ('Player 1', 25, 36, 1);
+INSERT INTO player (name, age, experience_months, team_id) VALUES ('Player 2', 22, 24, 2);
